97f8c50f8f9d9c33f0a335683dde4ed2d2977cf8
Laura <pinipinella@gmail.com>
2025-03-14 11:26:16 +0100

    Lauras Requierements

---
+++ Laura/png_zieher.py
@@ -0,0 +1,9 @@
+from jfhjdhs in hdhdhdhd
+	if .png
+
+
+alle charts in einem dingens anzeigen lassen -> bilderladen aus Vorlesung
+
+
+auf einen ordner zugreifen -> pngs holen
+zusammen in ein dashboard klatschen und abspeichern lassen und öffnen
